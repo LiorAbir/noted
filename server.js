e649e6405a39408d2cab95b616f77c2a3c944413
Lior Abir <liorabir909@gmail.com>
2023-02-09 19:26:48 +0200

    build public

--- server.js
+++ server.js
@@ -9,41 +9,44 @@ const http = require('http').createServer(app)
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-	const corsOptions = {
-		origin: [
-			'http://127.0.0.1:5173',
-			'http://127.0.0.1:8080',
-			'http://localhost:8080',
-			'http://127.0.0.1:3000',
-			'http://localhost:3000',
-		],
-		credentials: true,
-	}
-	app.use(cors(corsOptions))
+	app.use(express.static(path.resolve(__dirname, 'public')))
+	// const corsOptions = {
+	// 	origin: [
+	// 		'http://127.0.0.1:5173',
+	// 		'http://127.0.0.1:8080',
+	// 		'http://localhost:8080',
+	// 		'http://127.0.0.1:3000',
+	// 		'http://localhost:3030',
+	// 	],
+	// 	credentials: true,
+	// }
+	// app.use(cors(corsOptions))
 }
 
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const noteRoutes = require('./api/note/note.routes')
 const boardRoutes = require('./api/board/board.routes')
-// const {setupSocketAPI} = require('./services/socket.service')
+const { setupSocketAPI } = require('./services/socket.service')
 
 // routes
-// const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
-// app.all('*', setupAsyncLocalStorage)
+const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
+app.all('*', setupAsyncLocalStorage)
 
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/note', noteRoutes)
 app.use('/api/board', boardRoutes)
-// setupSocketAPI(http)
+setupSocketAPI(http)
 
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
+
 app.get('/**', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
@@ -51,11 +54,7 @@ app.get('/**', (req, res) => {
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
 http.listen(port, () => {
-	// logger.info('Server is running on port: ' + port)
-	logger.info(`Server listening on http://localhost:${port}`)
+	logger.info('Server is running on port: ' + port)
+	console.log(process.env.NODE_ENV)
+	// logger.info(`Server is ready at port: http://localhost:${port}/#/`)
 })
-
-//
-// "start": "set NODE_ENV=development&&nodemon server.js",
-// "server:dev": "set NODE_ENV=development&&nodemon server.js",
-// "server:prod": "set NODE_ENV=production&&node server.js"
