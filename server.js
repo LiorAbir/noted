9eb039e853c3d9372127bc2423b9a10b40641504
Lior Abir <liorabir909@gmail.com>
2023-02-19 23:13:04 +0200

    test

--- server.js
+++ server.js
@@ -10,23 +10,23 @@ const http = require('http').createServer(app)
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
-// 	const corsOptions = {
-// 		origin: [
-// 			'http://127.0.0.1:5173',
-// 			'http://localhost:5173',
-// 			'http://localhost',
-// 			'http://127.0.0.1:3000',
-// 			'http://localhost:3000',
-// 		],
-// 		credentials: true,
-// 	}
-// 	app.use(cors(corsOptions))
-// }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+	const corsOptions = {
+		origin: [
+			'http://127.0.0.1:5173',
+			'http://localhost:5173',
+			'http://localhost',
+			'http://127.0.0.1:3000',
+			'http://localhost:3000',
+		],
+		credentials: true,
+	}
+	app.use(cors(corsOptions))
+}
 
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
